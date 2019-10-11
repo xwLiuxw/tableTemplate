b3bded75ee7ffb1517aec208e3477effcecbcdc3
XUEWU.LIU <XUEWU.LIU@miniso.com>
2019-10-11 14:53:49 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div>api:<input v-model="api" /></div>
+        <textarea
+            v-model="value"
+            style="width:500px;height:500px"
+            placeholder='JAVA注解例子：
+            @ApiModelProperty(value = "商品sku") 
+            private String skuCode;'
+        />
+        <button @click="change">转换</button>
+        <textarea
+            v-model="tableConfig"
+            style="width:500px;height:500px"
+            v-if="false"
+        />
+        <textarea v-model="template" style="width:500px;height:500px" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    data () {
+        return {
+            value: '',
+            tableConfig: '',
+            temp: [],
+            template: '',
+            api: '',
+            demo: ''
+        }
+    },
+    created () {
+        this.$http.get('demo.txt').then(res => {
+            this.demo = res.body
+        })
+    },
+    methods: {
+        change () {
+            this.value.split('@')
+            let arr = this.value.split('@')
+            this.temp = []
+            arr.map(item => {
+                if (item) {
+                    this.temp.push({
+                        label: item.match(/(?<=").*?(?=")/)[0],
+                        prop: item.match(/(\S*);/)[1]
+                    })
+                }
+            })
+            this.tableConfig = JSON.stringify(this.temp)
+            this.template = this.demo.replace(/##/g, this.tableConfig)
+            this.template = this.template.replace(/&&/g, this.api)
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 }
 </style>
